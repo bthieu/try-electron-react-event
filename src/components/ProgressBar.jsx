f715d577460653d6258f4b61d69652959a7d9fcb
Hieu Bui <tranhieu0101@gmail.com>
2023-07-08 11:24:38 +0700

    use useGesture

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,33 +1,47 @@
 import React, { useState } from "react";
+import { useGesture } from '@use-gesture/react';
+
 
 function ProgressBar() {
   const [tooltipVisible, setTooltipVisible] = useState(false);
   const [tooltipPositionX, setTooltipPositionX] = useState(0);
 
-  const handleMouseEnter = (e) => {
-    setTooltipVisible(true);
-    updateTooltipPosition(e);
-  };
+  // const handleMouseEnter = (e) => {
+  //   setTooltipVisible(true);
+  //   updateTooltipPosition(e);
+  // };
 
-  const handleMouseLeave = () => {
-    setTooltipVisible(false);
-  };
+  // const handleMouseLeave = () => {
+  //   setTooltipVisible(false);
+  // };
 
-  const handleMouseMove = (e) => {
-    updateTooltipPosition(e);
-  };
+  // const handleMouseMove = (e) => {
+  //   updateTooltipPosition(e);
+  // };
 
-  const updateTooltipPosition = (e) => {
-    const { clientX } = e;
-    setTooltipPositionX(clientX);
-  };
+  // const updateTooltipPosition = (e) => {
+  //   const { clientX } = e;
+  //   setTooltipPositionX(clientX);
+  // };
+
+  const bind = useGesture({
+    onHover: ({ hovering, xy }) => {
+      setTooltipVisible(hovering);
+      setTooltipPositionX(xy[0]);
+    },
+    onMouseMove: ({event}) => {
+      const {clientX} = event;
+      setTooltipPositionX(clientX);
+    },
+  });
 
   return (
     <div
       className="tooltip-container"
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      onMouseMove={handleMouseMove}
+      // onMouseEnter={handleMouseEnter}
+      // onMouseLeave={handleMouseLeave}
+      // onMouseMove={handleMouseMove}
+      {...bind()}
     >
       <div className="tooltip-content">Hover over me!</div>
       {tooltipVisible && (
